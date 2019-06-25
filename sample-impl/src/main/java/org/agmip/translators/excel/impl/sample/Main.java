13f841f3051fa41a04fb0a80e1a201029c2f8ba1
Luis Felipe VARGAS ROJAS <vargasro@SUPAGRO.INRA.LAN>
2019-06-25 15:18:55 +0200

    upgrade translator to run with java 12 and ace-core 2.1.4

--- sample-impl/src/main/java/org/agmip/translators/excel/impl/sample/Main.java
+++ sample-impl/src/main/java/org/agmip/translators/excel/impl/sample/Main.java
@@ -2,6 +2,8 @@ package org.agmip.translators.excel.impl.sample;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+
 import org.agmip.translators.excel.api.ExcelModel;
 import org.agmip.ace.AceDataset;
 import org.agmip.ace.io.AceGenerator;
@@ -10,13 +12,28 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
   public static final Logger LOG = LoggerFactory.getLogger(Main.class);
-  public static void main(String[] argv) throws Exception {
+  public static void main(String[] argv)  {
+	  
+	
+	System.out.println(" I am felipe");
+	
+	if (argv.length==0) {
+		System.err.println("No args passed");
+		return;
+	}
     Path file = Paths.get(argv[0]);
     Path out  = Paths.get(argv[1]);
-    ExcelModel model = new ExcelModel(file);
-    model.init();
-    AceDataset ds = model.run();
-    LOG.info("Writing file: {}", out.toFile());
-    AceGenerator.generateACEB(out.toFile(), ds);
+    ExcelModel model;
+	try {
+		model = new ExcelModel(file);
+		model.init();
+	    AceDataset ds = model.run();
+	    LOG.info("Writing file: {}", out.toFile());
+	    AceGenerator.generateACEB(out.toFile(), ds);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    
   }
 }
